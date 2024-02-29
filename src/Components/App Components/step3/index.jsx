e801824b5b0a3fdfe1ef5993ddd5a0b5c657b4cb
georgielill <georgielills@gmail.com>
2024-02-29 20:16:52 +0000

    added bootstrap cards to step 3

--- src/Components/App Components/step3/index.jsx
+++ src/Components/App Components/step3/index.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function index() {
   return (
-    <div>Step3</div>
+    <div>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Body>
+      <Card.Title>Light</Card.Title>
+      <Card.Text>
+      Clean, modern, and elegant with green hues. 
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Body>
+      <Card.Title>Dark</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Body>
+      <Card.Title>Colourful</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  </div>
   )
 }
 
