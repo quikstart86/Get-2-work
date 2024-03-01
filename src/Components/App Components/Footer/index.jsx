7f4509b166a4f442ce6539754bac5d338caec834
Jonno Clifford <jma.clifford@gmail.com>
2024-03-01 22:47:22 +0000

    step 1 card layout worked on, navbar, footer styling

--- src/Components/App Components/Footer/index.jsx
+++ src/Components/App Components/Footer/index.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import '../Footer/footer.css';
+import './footer.css';
 
-function Index() {
+function Footer() {
   return (
-    <footer className="text-light py-3">
-      <div className='fixed-bottom'>
+    <footer className="footer">
       <Container>
         <Row>
-          <div className='footer-div'>
-          <Col xs={12} md={6}>
-            <h4>Contact Us </h4>
-            <p>Email: THIS IS PLACEHOLDER</p>
-            <p>Phone: THIS IS PLACEHOLDER</p>
-          </Col>
-          <Col xs={12} md={6}>
-            <h4>Follow Us</h4>
-            <p>Facebook | Twitter | Instagram</p>
-          </Col>
+          <div className="footer-div">
+            {/* <Col xs={12} md={6}>
+              <h4>Contact Us</h4>
+              <p>Email: THIS IS PLACEHOLDER</p>
+              <p>Phone: THIS IS PLACEHOLDER</p>
+            </Col> */}
+            {/* <Col xs={12} md={6}> */}
+              <h4>Follow Us</h4>
+              <p>Facebook | Twitter | Instagram</p>
+            {/* </Col> */}
           </div>
         </Row>
       </Container>
-      </div>
     </footer>
   );
 }
 
-export default Index;
+export default Footer;
